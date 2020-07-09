4085a5e35dcd63ff734305fbc4a35ca0c5ab96d5
Oelin <42823429+Oelin@users.noreply.github.com>
2020-07-09 14:38:21 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,35 +4,25 @@ const token = require('./token.js')
 const { spawn } = require('child_process')
 const prefix = '?'
 
+const ask = question => {
+  let stdin = Buffer.from(question).toString('base64')
+  return spawn('./answer.py', [stdin])
+}
 
-client.on('ready', () => {
-    console.log(`info: connected as ${client.user.tag}`)
-})
+const reply = ({author, content, channel}) => {
+  if (content.indexOf(prefix))
+    return
+  
+  if (author.bot) 
+    return
+  
+  let question = content.slice(1).trim()
+  let answer = ask(question)
+  
+  answer.stdout.on('data', data => 
+    channel.send(data.toString())
+  )
+}
 
-
-
-client.on('message', message => {
-    if (message.author.bot) {
-      return
-    }
-    
-    if (message.content.indexOf(prefix) !== 0) {
-      return
-    }
-
-    let question = message.content.slice(1).trim()
-    console.log(`info: someone asked a question - ${question}`)
-
-    let input = Buffer.from(question).toString('base64')
-    let answer = spawn('./ask.py', [input])
-    
-    answer.stdout.on('data', data => {
-      message.channel.send(data.toString())
-    })
-    
-    console.log(`info: answer was sent back`)
-})
-
-
-
+client.on('message', reply)
 client.login(token)
