be37e60707e52aa0f63a9f36e42be62a95b5aeb3
Oelin <42823429+Oelin@users.noreply.github.com>
2020-07-09 14:39:46 +0200

    Update and rename talk.py to answer.py

--- answer.py
+++ answer.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
 
-
-
 from requests import session, Request
 from requests.cookies import create_cookie
 from hashlib import md5
@@ -9,26 +7,18 @@ from urllib.parse import quote, unquote
 from sys import argv
 from base64 import b64decode as decode
 
-
-
 server = 'www.cleverbot.com'
 endpoint = 'webservicemin?uc=UseOfficialCleverbotAPI&'
 agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4094.0 Safari/537.36'
 
-
-
 def route(path=''):
     return f'https://{server}/{path}'
-
-
 
 # This is a token which must be valid for the request to work.
 
 def icogno(text):
     string = f's={quote(text)}' + '&cb_settings_language=en&cb_settings_scripting=no&islearning=1&icognoid=wsf'
     return md5(bytes(string[:26], 'utf-8')).hexdigest()
-
-
 
 # Ask a question to Cleverbot using a POST request.
 
@@ -71,10 +61,6 @@ def ask(text):
 
     return answer
 
-
-
-# Note: the question is base64 encoded to avoid command injection.
-
 def main():
     try: 
       question = str(decode(argv[1]), 'utf-8')
@@ -84,6 +70,4 @@ def main():
     except:
       print('Whoops')
 
-
-    
 main()
